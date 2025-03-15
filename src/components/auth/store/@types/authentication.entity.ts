6e3e60d665c7b13f186b2509edd822cc3bae3a94
Joel Vargas <vermudo.com@gmail.com>
2025-03-15 16:11:16 -0600

    refactor: remove unused authentication store and related hooks

--- src/components/auth/store/@types/authentication.entity.ts
+++
@@ -1,7 +0,0 @@
-export interface AuthenticationGlobalStore {
-  address: string;
-  name: string;
-
-  connectWalletStore: (address: string, name: string) => void;
-  disconnectWalletStore: () => void;
-}
