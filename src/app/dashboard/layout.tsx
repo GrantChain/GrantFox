db88a912674fbc3a8568b0e4f9fac06b38aa5626
Joel Vargas <vermudo.com@gmail.com>
2025-04-23 18:10:28 -0600

    feat: integrate MobileTrigger component into layout for improved sidebar functionality

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,6 +3,7 @@
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/layout/sidebar/app-sidebar";
 import useLayoutDashboard from "@/hooks/layout-dashboard.hook";
+import { MobileTrigger } from "@/components/layout/sidebar/mobile-trigger";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const { label } = useLayoutDashboard();
@@ -11,9 +12,13 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <div className="bg-muted/50 min-h-screen">
+        <div className="min-h-screen">
           <div className="flex-1 space-y-4 p-4 pt-6 md:p-8 h-full">
-            <h2 className="text-3xl font-bold tracking-tight">{label}</h2>
+            <div className="flex gap-4">
+              <MobileTrigger />
+              <h2 className="text-3xl font-bold tracking-tight">{label}</h2>
+            </div>
+
             {children}
           </div>
         </div>
