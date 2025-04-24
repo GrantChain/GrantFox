c2b8311a7aed8765f0aedcb62ea1e0da0439c6be
Joel Vargas <vermudo.com@gmail.com>
2025-04-23 19:13:45 -0600

    refactor: remove unused supabase integration from Page component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,6 @@
 "use client";
 
-import { supabase } from "@/lib/supabase";
-
 export default function Page() {
-  const handleSubmit = async () => {
-    const { data, error } = await supabase
-      .from("users")
-      .insert({ name: "John Doe" });
-
-    if (data) console.log(data);
-    if (error) console.error(error);
-  };
-
-  handleSubmit();
-
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
