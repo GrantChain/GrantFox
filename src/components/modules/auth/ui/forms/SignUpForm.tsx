c709a2baa4f68e7e4491a69c4b2d846207c49618
Joel Vargas <vermudo.com@gmail.com>
2025-04-23 19:14:08 -0600

    fix: update greeting message from "Hi!" to "Welcome!" in SignUpForm component

--- src/components/modules/auth/ui/forms/SignUpForm.tsx
+++ src/components/modules/auth/ui/forms/SignUpForm.tsx
@@ -32,7 +32,7 @@ export function SignUpForm({
             >
               <div className="flex flex-col gap-6">
                 <div className="flex flex-col items-center text-center">
-                  <h1 className="text-2xl font-bold">Hi!</h1>
+                  <h1 className="text-2xl font-bold">Welcome!</h1>
                   <p className="text-balance text-muted-foreground">
                     SignUp in GrantFox
                   </p>
