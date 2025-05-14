073304ee41debf74ccc1d743c9a4927f7cee6db0
Joel Vargas <vermudo.com@gmail.com>
2025-05-14 17:31:26 -0600

    feat: integrate UserProvider and RoleSelectionProvider into dashboard layout for improved context management

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,27 +5,33 @@ import { AppSidebar } from "@/components/layout/sidebar/app-sidebar";
 import useLayoutDashboard from "@/hooks/layout-dashboard.hook";
 import { MobileTrigger } from "@/components/layout/sidebar/mobile-trigger";
 import { Footer } from "@/components/layout/footer";
+import { RoleSelectionProvider } from "@/components/providers/role-selection.provider";
+import { UserProvider } from "@/components/modules/auth/context/UserContext";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const { label } = useLayoutDashboard();
 
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <div className="min-h-screen flex flex-col">
-          <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
-            <div className="flex gap-4">
-              <MobileTrigger />
-              <h2 className="text-3xl font-bold tracking-tight">{label}</h2>
-            </div>
+    <UserProvider>
+      <RoleSelectionProvider>
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <div className="min-h-screen flex flex-col">
+              <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
+                <div className="flex gap-4">
+                  <MobileTrigger />
+                  <h2 className="text-3xl font-bold tracking-tight">{label}</h2>
+                </div>
 
-            {children}
-          </div>
-          <Footer />
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+                {children}
+              </div>
+              <Footer />
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
+      </RoleSelectionProvider>
+    </UserProvider>
   );
 };
 
--- src/components/providers/global.provider.tsx
+++ src/components/providers/global.provider.tsx
@@ -1,13 +1,10 @@
 import { ThemeProvider } from "@/hooks/theme-provider";
 import { TanstackProvider } from "./tanstack.provider";
-import { RoleSelectionProvider } from "./role-selection.provider";
 
 export const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider defaultTheme="system">
-      <TanstackProvider>
-        <RoleSelectionProvider>{children}</RoleSelectionProvider>
-      </TanstackProvider>
+      <TanstackProvider>{children}</TanstackProvider>
     </ThemeProvider>
   );
 };
