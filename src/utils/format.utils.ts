f0fdd0357b906ac722265eae861948e70b4c427e
Joel Vargas <vermudo.com@gmail.com>
2025-06-09 14:16:10 -0600

    feat: add formatCurrency utility function for consistent currency formatting

---
+++ src/utils/format.utils.ts
@@ -0,0 +1,10 @@
+import { Decimal } from "@prisma/client/runtime/library";
+
+export const formatCurrency = (currency: string, amount: Decimal) => {
+  const formattedAmount = new Intl.NumberFormat("en-US", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(Number(amount));
+
+  return `${currency} ${formattedAmount}`;
+};
