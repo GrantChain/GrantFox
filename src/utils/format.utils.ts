a482f4794decf9d817bf0876609e0be325c618f1
Joel Vargas <vermudo.com@gmail.com>
2025-06-09 14:16:26 -0600

    refactor: change import of Decimal to type import for better type clarity

--- src/utils/format.utils.ts
+++ src/utils/format.utils.ts
@@ -1,4 +1,4 @@
-import { Decimal } from "@prisma/client/runtime/library";
+import type { Decimal } from "@prisma/client/runtime/library";
 
 export const formatCurrency = (currency: string, amount: Decimal) => {
   const formattedAmount = new Intl.NumberFormat("en-US", {
