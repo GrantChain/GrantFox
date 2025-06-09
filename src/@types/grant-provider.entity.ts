bb3cf13484a1a41474d131dfa3ddce5dc7c4705b
Joel Vargas <vermudo.com@gmail.com>
2025-06-09 09:50:24 -0600

    refactor: remove deprecated entity interfaces and enums for codebase cleanup

--- src/@types/grant-provider.entity.ts
+++
@@ -1,8 +0,0 @@
-export interface GrantProvider {
-  user_id: string;
-  organization_name?: string;
-  network_type?: string;
-  email?: string;
-  created_at: Date;
-  updated_at: Date;
-}
--- src/@types/grantee.entity.ts
+++
@@ -1,9 +0,0 @@
-export interface Grantee {
-  user_id: string;
-  name?: string;
-  position_title?: string;
-  social_media?: Record<string, any>;
-  linked_team_id?: string;
-  created_at: Date;
-  updated_at: Date;
-}
