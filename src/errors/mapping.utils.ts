d346f71a9658e040e86ad7bab3bb830d1845bdc6
Joel Vargas <vermudo.com@gmail.com>
2025-06-12 16:17:29 -0600

    feat: add error handling types and utilities for improved error management

--- src/errors/mapping.utils.ts
+++ src/errors/mapping.utils.ts
@@ -1,3 +1,49 @@
+import type {
+  ApiError,
+  ErrorResponse,
+  WalletError,
+} from "@/@types/error.entity";
+import axios, { type AxiosError } from "axios";
+import { ApiErrorTypes } from "./enums/error.enum";
+
+export const handleError = (error: AxiosError | WalletError): ErrorResponse => {
+  if (axios.isAxiosError(error)) {
+    const axiosError = error as AxiosError<ApiError>;
+    const code = axiosError.response?.status || 500;
+    const message = axiosError.response?.data?.message || error.message;
+    return {
+      message,
+      code,
+      type: mapStatusCodeToErrorType(code),
+    };
+  }
+
+  if (error.code === -4) {
+    return {
+      message: "Wallet was closed before transaction was sent",
+      code: -4,
+      type: ApiErrorTypes.WALLET_ERROR,
+    };
+  }
+
+  return {
+    message: error.message,
+    code: 500,
+    type: ApiErrorTypes.UNKNOWN_ERROR,
+  };
+};
+
+const mapStatusCodeToErrorType = (code: number): ApiErrorTypes => {
+  switch (code) {
+    case 404:
+      return ApiErrorTypes.NOT_FOUND;
+    case 401:
+      return ApiErrorTypes.UNAUTHORIZED;
+    default:
+      return ApiErrorTypes.UNKNOWN_ERROR;
+  }
+};
+
 export function extractErrorMessage(error: unknown): string {
   if (
     typeof error === "object" &&
