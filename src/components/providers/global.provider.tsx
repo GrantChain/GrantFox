39dd5d23693140af1ade7ee0abc30915d3464970
Joel Vargas <vermudo.com@gmail.com>
2025-06-12 15:19:47 -0600

    feat: setup trustless-work configuration

--- src/components/providers/global.provider.tsx
+++ src/components/providers/global.provider.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { ThemeProvider } from "@/components/providers/theme.provider";
-import { PayoutContextProvider } from "../modules/payouts/context/PayoutContext";
 import { PostHogProvider } from "./posthog.provider";
 import { TanstackProvider } from "./tanstack.provider";
+import { TrustlessWorkProvider } from "./trustless-work.provider";
 
 export const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider defaultTheme="system">
       <PostHogProvider>
         <TanstackProvider>
-          <PayoutContextProvider>{children}</PayoutContextProvider>
+          <TrustlessWorkProvider>{children}</TrustlessWorkProvider>
         </TanstackProvider>
       </PostHogProvider>
     </ThemeProvider>
