f70f67d69c13082c1f71a4f3bad95f1111d959db
Joel Vargas <vermudo.com@gmail.com>
2025-06-25 13:24:33 -0600

    chore: add package-lock.json for dependency management and update layout spacing in dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,7 +16,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           <SidebarInset>
             <Header />
             <div className="min-h-screen flex flex-col">
-              <div className="flex-1 space-y-4 p-4 pt-6 md:p-8 h-full">
+              <div className="flex-1 space-y-3 p-4 pt-6 md:p-8 h-full">
                 {children}
               </div>
             </div>
