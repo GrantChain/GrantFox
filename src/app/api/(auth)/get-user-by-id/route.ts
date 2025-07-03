bb09c14910ab0ad25adf905a4459720c4bc8015a
villarley <santivillarley1010@gmail.com>
2025-07-03 16:11:14 -0600

    refactor: remove console log from get-user-by-id route for cleaner code

--- src/app/api/(auth)/get-user-by-id/route.ts
+++ src/app/api/(auth)/get-user-by-id/route.ts
@@ -44,7 +44,6 @@ export async function GET(request: Request) {
         { status: 404 },
       );
     }
-    console.log(user);
     return NextResponse.json({ exists: true, user });
   } catch (error) {
     console.error("Error checking user:", error);
