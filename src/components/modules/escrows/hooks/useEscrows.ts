1b51f87618751b09af0803826b3deb73b85acff8
Joel Vargas <vermudo.com@gmail.com>
2025-08-07 18:19:11 -0600

    feat: implement useEscrows hook for managing escrow deployment and transaction handling

--- src/components/modules/escrows/hooks/useEscrows.ts
+++ src/components/modules/escrows/hooks/useEscrows.ts
@@ -3,23 +3,31 @@ import { useGlobalWalletStore } from "@/components/wallet/store/store";
 import { handleError } from "@/errors/mapping.utils";
 import { signTransaction } from "@/lib/wallet-kit";
 import {
-  useInitializeEscrow as useInitializeEscrowHook,
+  useFundEscrow,
+  useInitializeEscrow,
   useSendTransaction,
 } from "@trustless-work/escrow/hooks";
-import type { InitializeMultiReleaseEscrowPayload } from "@trustless-work/escrow/types";
+import type {
+  FundEscrowPayload,
+  InitializeMultiReleaseEscrowPayload,
+} from "@trustless-work/escrow/types";
 import type { AxiosError } from "axios";
 import { useState } from "react";
 import { toast } from "sonner";
 
-export const useInitializeMultiEscrowForm = () => {
+export const useEscrows = () => {
   const [loading, setLoading] = useState(false);
 
-  const { deployEscrow } = useInitializeEscrowHook();
+  const { address } = useGlobalWalletStore();
   const { sendTransaction } = useSendTransaction();
 
-  const { address } = useGlobalWalletStore();
+  const { deployEscrow } = useInitializeEscrow();
+  const { fundEscrow } = useFundEscrow();
 
-  const onSubmit = async (
+  // all the escrows list from the indexer endpoint
+  //
+
+  const handleDeployEscrow = async (
     payload: InitializeMultiReleaseEscrowPayload,
   ): Promise<boolean> => {
     setLoading(true);
@@ -66,8 +74,54 @@ export const useInitializeMultiEscrowForm = () => {
       setLoading(false);
     }
   };
+
+  // const handleFundEscrow = async (payload: FundEscrowPayload) => {
+  //   setLoading(true);
+
+  //   try {
+  //     // This is the final payload that will be sent to the API
+  //     const finalPayload: FundEscrowPayload = {
+  //       ...payload,
+  //       signer: address || "",
+  //     };
+
+  //     const { unsignedTransaction } = await deployEscrow(
+  //       finalPayload,
+  //       "multi-release",
+  //     );
+
+  //     if (!unsignedTransaction) {
+  //       throw new Error(
+  //         "Unsigned transaction is missing from deployEscrow response.",
+  //       );
+  //     }
+
+  //     const signedXdr = await signTransaction({
+  //       unsignedTransaction,
+  //       address: address || "",
+  //     });
+
+  //     if (!signedXdr) {
+  //       throw new Error("Signed transaction is missing.");
+  //     }
+
+  //     await sendTransaction(signedXdr);
+  //     return true;
+  //   } catch (error: unknown) {
+  //     const mappedError = handleError(error as AxiosError | WalletError);
+  //     console.error("Error:", mappedError.message);
+
+  //     toast.error(
+  //       mappedError ? mappedError.message : "An unknown error occurred",
+  //     );
+  //     return false;
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
   return {
     loading,
-    onSubmit,
+    handleDeployEscrow,
   };
 };
