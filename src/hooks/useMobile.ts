fa1654ac0732f4781f2930ebc611377a27c2010b
Joel Vargas <vermudo.com@gmail.com>
2025-08-13 14:36:10 -0600

    feat: add useIsTabletOrBelow hook to determine tablet or smaller screen sizes

--- src/hooks/useMobile.ts
+++ src/hooks/useMobile.ts
@@ -19,3 +19,23 @@ export function useIsMobile() {
 
   return !!isMobile;
 }
+
+const TABLET_BREAKPOINT = 1024;
+
+export function useIsTabletOrBelow() {
+  const [isTabletOrBelow, setIsTabletOrBelow] = React.useState<
+    boolean | undefined
+  >(undefined);
+
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${TABLET_BREAKPOINT - 1}px)`);
+    const onChange = () => {
+      setIsTabletOrBelow(window.innerWidth < TABLET_BREAKPOINT);
+    };
+    mql.addEventListener("change", onChange);
+    setIsTabletOrBelow(window.innerWidth < TABLET_BREAKPOINT);
+    return () => mql.removeEventListener("change", onChange);
+  }, []);
+
+  return !!isTabletOrBelow;
+}
