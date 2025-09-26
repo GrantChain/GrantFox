dd340c67608e853be08f1035c8be4b81a3604bae
gr4yx01 <hoyx0101@gmail.com>
2025-09-26 16:26:24 +0100

    fix: implement validation schemas for bountyApplication and comment models

--- src/components/modules/bounty/schema/bounty-application.schema.ts
+++ src/components/modules/bounty/schema/bounty-application.schema.ts
@@ -2,13 +2,11 @@ import { z } from "zod";
 
 const ApplicationStatus = z.enum(["PENDING", "APPROVED", "REJECTED"]);
 
-const cuidRegex = /^[a-z0-9]{25}$/;
-
 export const bountyApplicationCreateSchema = z.object({
-  payout_id: z.string().regex(cuidRegex, {
+  payout_id: z.string().uuid({
     message: "Invalid user ID format (must be valid CUID)",
   }),
-  grantee_id: z.string().regex(cuidRegex, {
+  grantee_id: z.string().uuid({
     message: "Invalid user ID format (must be valid CUID)",
   }),
   application_status: ApplicationStatus,
--- src/components/modules/comment/schema/comment.schema.ts
+++ src/components/modules/comment/schema/comment.schema.ts
@@ -1,16 +1,14 @@
 import { z } from "zod";
 
-const cuidRegex = /^[a-z0-9]{25}$/;
-
 export const commentCreateSchema = z.object({
   message: z
     .string()
     .min(10, "Message must be at least 10 characters")
-    .max(500, "Message must be at least 500 characters"),
-  user_id: z.string().regex(cuidRegex, {
+    .max(500, "Message must be at most 500 characters"),
+  user_id: z.string().uuid({
     message: "Invalid user ID format (must be valid CUID)",
   }),
-  payout_id: z.string().regex(cuidRegex, {
+  payout_id: z.string().uuid({
     message: "Invalid payout ID format (must be valid CUID)",
   }),
 });
@@ -19,5 +17,5 @@ export const commentUpdateSchema = z.object({
   message: z
     .string()
     .min(10, "Message must be at least 10 characters")
-    .max(500, "Message must be at least 500 characters"),
+    .max(500, "Message must be at most 500 characters"),
 });
